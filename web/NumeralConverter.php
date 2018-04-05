ce210a5151c66231b45e425113312e540b830bb1
Eponick <mikehuntwife@gmail.com>
2018-04-05 18:26:37 -0400

    Whole project

---
+++ web/NumeralConverter.php
@@ -0,0 +1,93 @@
+<?php
+    final class NumeralConverter
+    {
+        public static $romanPairs = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        );
+
+        public static function CheckIfNumber($data)
+        {
+            if(is_int($data))
+            {
+                return true;
+            }
+            if(intval($data) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static function CheckIfRoman($data)
+        {
+            if(!is_string($data))
+            {
+                return false;
+            }
+            if(preg_replace('~[MDCLXVI]~', '', $data) == "")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static function ConvertToNumber($roman)
+        {
+            if(!static::CheckIfRoman($roman))
+            {
+                return 0;
+            }
+
+            $retval = 0;
+            foreach(static::$romanPairs as $key => $val)
+            {
+                while(strpos($roman, $key) === 0)
+                {
+                    $retval += $val;
+                    $roman = substr($roman, strlen($key));
+                }
+            }
+
+            if(strlen($roman) > 0)
+            {
+                return 0;
+            }
+            return $retval;
+        }
+
+        public static function ConvertToRoman($num)
+        {
+            if(!static::CheckIfNumber($num))
+            {
+                return "";
+            }
+        
+            $numeralString = "";
+            $decimal = array(1000, 500, 100, 50, 10, 5, 1);
+            $numeral = array("M", "D", "C", "L", "X", "V", "I");
+        
+            for($i = 0; $i < count($decimal); $i++)
+            {
+                while($num % $decimal[$i] < $num)
+                {
+                    $numeralString = $numeralString . $numeral[$i];
+                    $num -= $decimal[$i];
+                }
+            }
+
+            return $numeralString;
+        }
+    }
+?>
---
+++ web/NumeralConverterTest.php
@@ -0,0 +1,82 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+    require 'NumeralConverter.php';
+
+    final class NumeralConverterTest extends TestCase
+    {
+        /**
+         * @dataProvider argumentProvider
+         */
+        public function testCanCheckNumbers($arg, $expected)
+        {
+            $this->assertSame(NumeralConverter::CheckIfNumber($arg), $expected);
+        }
+        
+        public function argumentProvider()
+        {
+            return [
+                [32, true],
+                ["32", true],
+                ["hi", false],
+                [88, true]
+            ];
+        }
+
+        /**
+         * @dataProvider romanProvider
+         */
+        public function testCanCheckRoman($arg, $expected)
+        {
+            $this->assertSame(NumeralConverter::CheckIfRoman($arg), $expected);
+        }
+
+        public function romanProvider()
+        {
+            return [
+                [32, false],
+                ["32", false],
+                ["IV", true],
+                ["LXII", true],
+                ["LXHI", false]
+            ];
+        }
+
+        /**
+         * @dataProvider convertnumProvider
+         */
+        public function testCanConvertRomanToNum($arg, $expected)
+        {
+            $this->assertSame(NumeralConverter::ConvertToNumber($arg), $expected);
+        }
+
+        public function convertnumProvider()
+        {
+            return [
+                [32, 0],
+                ["32", 0],
+                ["IV", 4],
+                ["CXXVIII", 128],
+                ["LXHI", 0],
+                ["LXLXVVCLCLXIMMIX", 0]
+            ];
+        }
+
+        /**
+         * @dataProvider convertromProvider
+         */
+        public function testCanConvertRomanToRoman($arg, $expected)
+        {
+            $this->assertSame(NumeralConverter::ConvertToRoman($arg), $expected);
+        }
+
+        public function convertromProvider()
+        {
+            return [
+                [10, "X"],
+                ["15", "XV"],
+                ["HIMAX", ""],
+                [1325, "MCCCXXV"]
+            ];
+        }
+    }
+?>
