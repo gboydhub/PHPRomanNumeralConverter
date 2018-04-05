16e3db20ce90e7d6b8d6f63065d3856e1c046640
Eponick <mikehuntwife@gmail.com>
2018-04-05 13:33:50 -0400

    Converter class finished

--- NumeralConverter.php
+++ NumeralConverter.php
@@ -90,20 +90,4 @@
             return $numeralString;
         }
     }
-    /*
-    $usrNum = $_POST['usrInput'];
-    $numeralString = "";
-    $decimal = array(1000, 500, 100, 50, 10, 5, 1);
-    $numeral = array("M", "D", "C", "L", "X", "V", "I");
-
-    for($i = 0; $i < count($decimal); $i++)
-    {
-        while($usrNum % $decimal[$i] < $usrNum)
-        {
-            $numeralString = $numeralString . $numeral[$i];
-            $usrNum -= $decimal[$i];
-        }
-    }
-
-    echo $numeralString;*/
 ?>
